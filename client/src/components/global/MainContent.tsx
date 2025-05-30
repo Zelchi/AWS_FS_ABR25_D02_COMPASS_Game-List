4a25e082383b893dec24d33de52c0b5405a75459
Lorena Bastos <lorenaax.bastos@hotmail.com>
2025-05-30 00:59:47 -0300

    feat: create component main with searchbar placeholder

---
+++ client/src/components/global/MainContent.tsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import React, { ReactNode } from "react";
+import ToggleButton from "@/components/navigation/ToggleButton";
+import { useLocation } from "react-router-dom";
+import SearchBar from "@/components/global/SearchBar";
+
+const routes = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    path: "/games",
+    label: "Games",
+  },
+  {
+    path: "/categories",
+    label: "Categories",
+  },
+  {
+    path: "/platforms",
+    label: "Platforms",
+  },
+  {
+    path: "/login",
+    label: "Logout",
+  },
+];
+
+const Main = styled.main`
+  min-height: 100vh;
+  background-color: var(--color-grey-dark-02);
+`;
+
+const Title = styled.h1`
+  line-height: 1;
+  font-family: var(--font-primary);
+  font-weight: 700;
+  font-size: 2.2rem;
+  color: var(--color-white);
+`;
+
+const Container = styled.section`
+  padding: 2.1rem 3rem 3rem;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 3rem;
+`;
+
+const SearchContainer = styled.div`
+  margin-left: auto;
+`;
+
+export default function MainContent({
+  children,
+  isOpen,
+  onOpen,
+}: {
+  children: ReactNode;
+  isOpen: boolean;
+  onOpen: () => void;
+}) {
+  const path = useLocation().pathname;
+
+  return (
+    <Main>
+      <Container>
+        <Header>
+          <ToggleButton isOpen={isOpen} onOpen={onOpen} />
+          {path !== "/" && <Title>{routes.find((route) => route.path === path)?.label}</Title>}
+          {path === "/" && (
+            <SearchContainer>
+              <SearchBar />
+            </SearchContainer>
+          )}
+        </Header>
+        {children}
+      </Container>
+    </Main>
+  );
+}
---
+++ client/src/components/global/SearchBar.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Input = styled.input``;
+
+export default function SearchBar() {
+  return <Input placeholder="Search"></Input>;
+}
