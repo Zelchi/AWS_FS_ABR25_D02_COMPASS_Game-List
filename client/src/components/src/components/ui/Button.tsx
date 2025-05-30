1d085663197aa67c87c1340ad76ec6285600cf17
Letícia Brandizzi <132960000+TycyBrandizzi@users.noreply.github.com>
2025-05-30 14:36:47 -0300

    Update Button.tsx

--- client/src/components/src/components/ui/Button.tsx
+++ client/src/components/src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/src/lib/utils";
 import { ButtonHTMLAttributes } from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-
   {
     variants: {
       variant: {
@@ -29,5 +29,5 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 export default function Button({ className, variant, size, ...props }: ButtonProps) {
-  return <button className={cn(buttonVariants({ variant, size }), className)} {...props} />;
+  return <button className={cn(buttonVariants({ variant, size }), className)} {...pro
 }
