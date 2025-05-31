c0c4fdb95668b81520659bc2982b2982b88f3df0
Zelchi <tininhakawaii@gmail.com>
2025-05-31 13:34:09 -0300

    fix: integrate React Query for improved data fetching in main application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "@/styles/globals";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
