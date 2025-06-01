36b8885ef69b4ba5d337abfd9a1716f606c94511
Zelchi <tininhakawaii@gmail.com>
2025-06-01 13:32:00 -0300

    feat: modal component

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import Modal from "../components/global/Modal";
+
 export default function NotFound() {
-  return <div>Not Found</div>;
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setIsOpen(true)}>Abrir Modal</button>
+
+            <Modal
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+                title="New Game"
+                size="md"
+            >
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+
+                <div>
+                    <button onClick={() => setIsOpen(false)}>
+                        Fechar
+                    </button>
+                </div>
+            </Modal>
+        </>
+    );
 }
