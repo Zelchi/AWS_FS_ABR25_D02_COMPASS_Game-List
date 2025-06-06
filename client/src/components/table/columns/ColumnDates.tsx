924500fa860e4be62eb251e24498a5eac313b0ef
Zelchi <tininhakawaii@gmail.com>
2025-06-06 08:45:56 -0300

    fix: update date validation logic in ColumnDates to handle all years before 1980

--- client/src/components/table/columns/ColumnDates.tsx
+++ client/src/components/table/columns/ColumnDates.tsx
@@ -1,7 +1,7 @@
 export default function ColumnDates({ item, head }: { item: any; head: string }) {
   const date = new Date((item as any)[head]);
   
-  if (date.getFullYear() === 1969 && date.getMonth() === 11 && date.getDate() === 31) {
+  if (date.getFullYear() < 1980) {
     return <span></span>;
   }
   
